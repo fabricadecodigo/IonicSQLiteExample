4f4c1366e88253c818d7ded3976efc43fbeb130f
Felipe <feliperfranco@gmail.com>
2019-10-31 20:36:27 -0300

    ajustes finaus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      //this.db.openDatabase();
+      this.db.openDatabase();
     });
   }
 }
